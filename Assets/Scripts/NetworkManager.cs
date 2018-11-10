f19583b47cefb197aaa3bd9b2e3ac0d56bc68da7
nashvent <anibal.ventura@ucsp.edu.pe>
2018-11-09 20:44:56 -0500

    Deteccion de movimiento del usuario listo

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using SocketIO;
 
+
 public class NetworkManager : MonoBehaviour {
     public SocketIOComponent socket;
+    public GameObject cube;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(ConnectToServer());
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine(ConnectToServer());	
-	}
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(3, 3, 3);
+
+
+
+    }
 	
     IEnumerator ConnectToServer()
     {
@@ -19,6 +26,33 @@ public class NetworkManager : MonoBehaviour {
     }
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        StartCoroutine(updatePosition());
+    }
+
+    IEnumerator updatePosition()
+    {
+        yield return new WaitForSeconds(0.5f);
+        JSONObject j = new JSONObject(JSONObject.Type.OBJECT);
+        var pos = GameObject.Find("Head").transform.position;
+
+        j.AddField("x", pos.x);
+        j.AddField("y", pos.y);
+        j.AddField("z", pos.z);
+        
+        socket.Emit("position",j);
+
+    }
+
+    public void mensaje(string msj)
+    {
+
+    }
+
+    IEnumerator messageServer()
+    {
+        yield return new WaitForSeconds(0.5f);
+        //socket.On('connect',mensaje);
+    }
+
+
 }
--- nodeServe/index.js
+++ nodeServe/index.js
@@ -5,16 +5,20 @@ var io=require('socket.io')(server);
 server.listen(3000);
 //server.listen(3000,"192.168.1.10");
 app.get('/',function(req,res){
-	res.send('Chasho');
+	res.send('Unity server');
 });
 
 io.on('connection',function (socket){
 	socket.on('player connect',function(){
-		console.log('JUgador connectador')
+		console.log('JUgador conectado');
+	});
+
+	socket.on('position',function(data){
+		console.log(data);
 	});
 
 	socket.on('disconnect',function(){
-		console.log('JUgador desconecta')
+		console.log('JUgador desconecta');
 	});
 });
 
