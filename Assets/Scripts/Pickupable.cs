6b170630943c1e5990b14098ffc47de1596b0135
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2018-11-23 13:06:38 -0500

    agrego mover objeto

---
+++ Assets/Scripts/Pickupable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour {
+
+    public bool capturado = false;
+    // Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (capturado)
+        {
+            copyPosition();
+        }
+
+    }   
+    
+    public void copyPosition()
+    {
+        var pos = GameObject.Find("Head").transform.position;
+        Debug.Log(pos);
+        GameObject objeto;
+        objeto = GameObject.Find("pCube");
+        objeto.transform.position = new Vector3(pos[0],pos[1],pos[2]+3);        
+    }
+    public void capturar()
+    {
+        capturado = !capturado;
+    }
+}
